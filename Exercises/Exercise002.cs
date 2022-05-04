f17ec9aa66657dd0e1435945232fb2df8d19d29b
AjaSharma93 <aja.sharma1101@gmail.com>
2022-05-04 16:37:11 +0100

    Updated Exercise002
    Solutions for Exercise 2

--- Exercises/Exercise002.cs
+++ Exercises/Exercise002.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using Exercises.Models;
+using System;
 namespace Exercises
 {
     public class Exercise002
     {
-        public bool IsFromManchester(/* Person person */)
+        public bool IsFromManchester(Person person)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return person.City == "Manchester";
         }
 
-        public bool CanWatchFilm(/*Person person, int ageLimit*/)
+        public bool CanWatchFilm(Person person, int ageLimit)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return person.Age > ageLimit;
         }
     }
 }
