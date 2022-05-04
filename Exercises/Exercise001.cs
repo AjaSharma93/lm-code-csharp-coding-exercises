f5450eb8a9a090724f82498d15c01360d0f84dac
AjaSharma93 <aja.sharma1101@gmail.com>
2022-05-04 15:21:34 +0100

    Update Excercise001.cs
    Added solutions for Exercise 1

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -9,31 +9,46 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            string result = "";
+            if(word != null && word!="")
+            {
+                result = word[0].ToString().ToUpper()+word.Substring(1);
+            }
+            return result;
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            string result = "";
+            if(firstName != null && firstName.Length > 0) result = result + firstName[0];
+            if(lastName != null && lastName.Length > 0) result = result + "." + lastName[0];
+            return result;
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            return Math.Round((1 + vatRate/100) * originalPrice,2);
         }
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            char[] chars = sentence.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
         }
 
         public int CountLinuxUsers(List<User> users)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            int countUsers = 0;
+            users.ForEach(user =>
+            {
+                if (user.Type == "Linux")
+                    countUsers++;
+            });
+            return countUsers;
         }
     }
 }
